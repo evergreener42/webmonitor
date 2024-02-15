f861e5b4187f029643f80ce02e15b5495234abbb
John Schmidt <johnismobile@hotmail.com>
2024-02-15 10:26:59 -0800

    added twilio

--- webpage.py
+++ webpage.py
@@ -1,21 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
-
 def search_for_whale(url):
     try:
         # Fetch the webpage content
-        response = requests.get(url)
+
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+
+        response = requests.get(url, headers=headers)
+        #response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
 
+#        with open("output.txt", "w") as file:
+#            file.write(str(soup))  # Convert soup to a string before writing
+
         # Search for the string "whale" in the page
-        if 'whale' in soup.get_text().lower():
-            send_email()
-            print("Found 'whale'! Email sent to jds@example.com.")
+        if 'Orca' in soup.get_text().lower():
+            send_email('Orca')
+            print("Found 'Orca whale' in West Seattle Blog.")
         else:
-            print("No 'whale' found on the webpage.")
+            print("No 'orca whale' found on the webpage.")
+        if 'orca' in soup.get_text().lower():
+            send_email('Orca')
+            print("Found 'Orca whale' in West Seattle Blog.")
+        else:
+            print("No 'Orca whale' found on the webpage.")
+        if 'umpback' in soup.get_text().lower():
+            send_email('Humpback')
+            print("Found 'humpback whale' in West Seattle Blog.")
+        else:
+            print("No 'humpback whale' found on the webpage.")
+
     except Exception as e:
         print(f"Error: {e}")
+
 
 def send_email():
     # Replace with your actual email configuration
@@ -42,5 +60,6 @@ def send_email():
 # Example usage
 if __name__ == "__main__":
     webpage_url = "https://www.orcanetwork.org/recent-sightings"  # Replace with the actual webpage URL
+    webpage_url = "https://westseattleblog.com/"  # Replace with the actual webpage URL
     search_for_whale(webpage_url)
 
