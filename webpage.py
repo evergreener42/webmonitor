39533f6e379bb0f508fa6f0cf889dc5b5146cb4b
John Schmidt <johnismobile@hotmail.com>
2024-02-15 13:55:50 -0800

    removed smtp

--- webpage.py
+++ webpage.py
@@ -1,6 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
+import subprocess
+
+def run_linux_command(command):
+    try:
+        # Execute the Linux command
+        command = "/home/johns/webmonitor/twilio.sh"
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        
+        # Print the output (stdout and stderr)
+        print("Command output:")
+        print(result.stdout)
+        print("Error (if any):")
+        print(result.stderr)
+    except Exception as e:
+        print(f"Error executing the command: {e}")
+
+
 def search_for_whale(url):
     try:
         # Fetch the webpage content
@@ -11,22 +28,22 @@ def search_for_whale(url):
         #response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-#        with open("output.txt", "w") as file:
-#            file.write(str(soup))  # Convert soup to a string before writing
+        with open("output.txt", "w") as file:
+            file.write(str(soup))  # Convert soup to a string before writing
 
         # Search for the string "whale" in the page
-        if 'Orca' in soup.get_text().lower():
-            send_email('Orca')
+        if 'Whale' in soup.get_text().lower():
+            run_linux_command('Orca')
             print("Found 'Orca whale' in West Seattle Blog.")
         else:
             print("No 'orca whale' found on the webpage.")
         if 'orca' in soup.get_text().lower():
-            send_email('Orca')
+            run_linux_command('Orca')
             print("Found 'Orca whale' in West Seattle Blog.")
         else:
             print("No 'Orca whale' found on the webpage.")
         if 'umpback' in soup.get_text().lower():
-            send_email('Humpback')
+            run_linux_command('Humpback')
             print("Found 'humpback whale' in West Seattle Blog.")
         else:
             print("No 'humpback whale' found on the webpage.")
@@ -37,25 +54,6 @@ def search_for_whale(url):
 
 def send_email():
     # Replace with your actual email configuration
-    smtp_server = 'smtp.example.com'
-    smtp_port = 587
-    sender_email = 'your_email@example.com'
-    sender_password = 'your_email_password'
-    recipient_email = 'jds@example.com'
-
-    try:
-        server = smtplib.SMTP(smtp_server, smtp_port)
-        server.starttls()
-        server.login(sender_email, sender_password)
-
-        subject = "Whale Alert!"
-        body = "The string 'whale' was found on the webpage."
-
-        message = f"Subject: {subject}\n\n{body}"
-        server.sendmail(sender_email, recipient_email, message)
-        server.quit()
-    except Exception as e:
-        print(f"Error sending email: {e}")
 
 # Example usage
 if __name__ == "__main__":
